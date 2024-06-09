727d57b431a9bad851e08ce647b229ce80125d64
Lovis Bero Suchmann <lovis.suchmann@rwth-aachen.de>
2024-06-09 05:32:05 -0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import { ecsstatic } from "@acab/ecsstatic/vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://astro.build/config
+export default defineConfig({
+  site: "http://localhost:4321/",
+  // Enable React to support React JSX components.
+  integrations: [react()],
+  vite: {
+    plugins: [ecsstatic()],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
